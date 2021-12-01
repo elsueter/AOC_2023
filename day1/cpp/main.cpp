77031d2c375c8d558be02300e07c0ab816f626a4
Elsueter <joseph_straw@outlook.com>
2021-12-01 23:49:42 +0000

    Consolidated to single file

--- day1/cpp/main.cpp
+++ day1/cpp/main.cpp
@@ -1,4 +1,34 @@
-#include "common.h"
+#include <iostream>
+
+#include <string>
+#include <vector>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+std::vector<int> readFile(){
+
+    std::vector<int> input;
+    input.reserve(2000);
+
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen("../input.txt", "r");
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        input.push_back(atoi(line));
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+
+    return input;
+}
 
 int part1(std::vector<int> input){
     int last = 0;
@@ -29,21 +59,19 @@ int part2(std::vector<int> input){
 }
 
 int main(){
-    timer clock;
-    clock.start();
+    clock_t start, end;
+
+    start = clock();
 
     std::vector<int> input = readFile();
-
-    clock.stop();
-    clock.start();
-
     int pt1 = part1(input);
     int pt2 = part2(input);
 
-    clock.stop();
+    end = clock();
 
     std::cout<<"Part 1 result: "<<pt1<<std::endl;
     std::cout<<"Part 2 result: "<<pt2<<std::endl;
+    std::cout<<"Took: "<<(((double) (end - start)) * 1000000000) / CLOCKS_PER_SEC<<"ns"<<std::endl;
 
     return 0;
 }
