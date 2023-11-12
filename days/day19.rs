b8c233c761f3596362886108ad95a6997512fe05
Joseph Straw <joseph_straw@outlook.com>
2023-11-12 06:20:29 +0000

    cleanup and fixed setup.sh

--- days/day19.rs
+++
@@ -1,4 +0,0 @@
-use crate::utils;
-use crate::return_sol;
-pub fn part1(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}
-pub fn part2(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}
--- setup.sh
+++ setup.sh
@@ -23,3 +23,29 @@ echo 'use crate::utils;\nuse crate::return_sol;\npub fn part1(lines:&Vec<String>
 echo 'use crate::utils;\nuse crate::return_sol;\npub fn part1(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}\npub fn part2(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}' >days/day23.rs
 echo 'use crate::utils;\nuse crate::return_sol;\npub fn part1(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}\npub fn part2(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}' >days/day24.rs
 echo 'use crate::utils;\nuse crate::return_sol;\npub fn part1(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}\npub fn part2(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}' >days/day25.rs
+
+echo '' >input/day01_input.txt
+echo '' >input/day02_input.txt
+echo '' >input/day03_input.txt
+echo '' >input/day04_input.txt
+echo '' >input/day05_input.txt
+echo '' >input/day06_input.txt
+echo '' >input/day07_input.txt
+echo '' >input/day08_input.txt
+echo '' >input/day09_input.txt
+echo '' >input/day10_input.txt
+echo '' >input/day11_input.txt
+echo '' >input/day12_input.txt
+echo '' >input/day13_input.txt
+echo '' >input/day14_input.txt
+echo '' >input/day15_input.txt
+echo '' >input/day16_input.txt
+echo '' >input/day17_input.txt
+echo '' >input/day18_input.txt
+echo '' >input/day19_input.txt
+echo '' >input/day20_input.txt
+echo '' >input/day21_input.txt
+echo '' >input/day22_input.txt
+echo '' >input/day23_input.txt
+echo '' >input/day24_input.txt
+echo '' >input/day25_input.txt
