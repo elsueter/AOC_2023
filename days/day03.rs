8965fbf934873fcfcf193146f01d0a637b635793
elsueter <joseph_straw@outlook.com>
2024-12-10 15:48:15 +0000

    day 3 part 2 solution

--- days/day03.rs
+++ days/day03.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 
 pub fn part1(lines: &Vec<String>) -> utils::Solution {
     let mut result = 0;
-    let regex = Regex::new(r"mul\([0-9]*,[0-9]*\)").unwrap();
+    let regex = Regex::new(r"mul\([0-9]+,[0-9]+\)").unwrap();
     for line in lines {
         let results: Vec<&str> = regex.find_iter(line).map(|m| m.as_str()).collect();
         for instr in results {
@@ -19,5 +19,30 @@ pub fn part1(lines: &Vec<String>) -> utils::Solution {
     return_sol!(result)
 }
 pub fn part2(lines: &Vec<String>) -> utils::Solution {
-    return_sol!(0)
+    let mut result = 0;
+    let regex = Regex::new(r"mul\([0-9]+,[0-9]+\)|don't\(\)|do\(\)").unwrap();
+    let mut enabled = true;
+    for line in lines {
+        let results: Vec<&str> = regex.find_iter(line).map(|m| m.as_str()).collect();
+        for instr in results {
+            match instr.get(..3) {
+                Some("don") => enabled = false,
+                Some("do(") => enabled = true,
+                Some("mul") => {
+                    if enabled {
+                        let results: Vec<&str> =
+                            regex.find_iter(line).map(|m| m.as_str()).collect();
+                        let num_regex = Regex::new(r"[0-9]+").unwrap();
+                        let nums: Vec<u32> = num_regex
+                            .find_iter(instr)
+                            .map(|m| m.as_str().parse::<u32>().unwrap())
+                            .collect();
+                        result += nums[0] * nums[1];
+                    }
+                }
+                _ => println!("err"),
+            }
+        }
+    }
+    return_sol!(result)
 }
