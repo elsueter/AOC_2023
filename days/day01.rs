f789a918885d065858768e20d7203d37f745cdad
elsueter <joseph_straw@outlook.com>
2024-12-10 02:47:50 +0000

    implemented day 1 solution

--- days/day01.rs
+++ days/day01.rs
@@ -1,8 +1,35 @@
 use crate::return_sol;
 use crate::utils;
 pub fn part1(lines: &Vec<String>) -> utils::Solution {
-    return_sol!(0)
+    let mut lhs: Vec<u32> = vec![];
+    let mut rhs: Vec<u32> = vec![];
+    for line in lines {
+        let mut split_line = line.split("   ");
+        lhs.push(split_line.next().unwrap().parse::<u32>().unwrap());
+        rhs.push(split_line.next().unwrap().parse::<u32>().unwrap());
+
+        lhs.sort();
+        rhs.sort();
+    }
+
+    let mut result = 0;
+    lhs.iter()
+        .enumerate()
+        .for_each(|(i, x)| result += u32::abs_diff(*x, rhs[i]));
+    return_sol!(result)
 }
 pub fn part2(lines: &Vec<String>) -> utils::Solution {
-    return_sol!(0)
+    let mut lhs: Vec<u32> = vec![];
+    let mut rhs: Vec<u32> = vec![];
+    for line in lines {
+        let mut split_line = line.split("   ");
+        lhs.push(split_line.next().unwrap().parse::<u32>().unwrap());
+        rhs.push(split_line.next().unwrap().parse::<u32>().unwrap());
+    }
+
+    let mut result = 0;
+    for num in lhs.iter() {
+        result += num * rhs.iter().filter(|x| *x == num).count() as u32;
+    }
+    return_sol!(result)
 }
