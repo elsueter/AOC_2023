9e4354bf8a7c36f80aaed4eeb2cfd804e8f971e9
elsueter <joseph_straw@outlook.com>
2024-12-10 03:47:04 +0000

    day 3 part 1 implementation

--- days/day03.rs
+++ days/day03.rs
@@ -1,4 +1,23 @@
-use crate::utils;
 use crate::return_sol;
-pub fn part1(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}
-pub fn part2(lines:&Vec<String>) -> utils::Solution{return_sol!(0)}
+use crate::utils;
+use regex::Regex;
+
+pub fn part1(lines: &Vec<String>) -> utils::Solution {
+    let mut result = 0;
+    let regex = Regex::new(r"mul\([0-9]*,[0-9]*\)").unwrap();
+    for line in lines {
+        let results: Vec<&str> = regex.find_iter(line).map(|m| m.as_str()).collect();
+        for instr in results {
+            let num_regex = Regex::new(r"[0-9]+").unwrap();
+            let nums: Vec<u32> = num_regex
+                .find_iter(instr)
+                .map(|m| m.as_str().parse::<u32>().unwrap())
+                .collect();
+            result += nums[0] * nums[1];
+        }
+    }
+    return_sol!(result)
+}
+pub fn part2(lines: &Vec<String>) -> utils::Solution {
+    return_sol!(0)
+}
